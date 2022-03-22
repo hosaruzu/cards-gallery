66f92f2e0587d9df567d8441c1312b53668999c6
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-03-22 19:59:30 +0400

    Add plugin for active card by default

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slider__item')
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slider__item')
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('slider__item--active')
-  })
+  slides[activeSlide].classList.add('slider__item--active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('slider__item--active')
+    })
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('slider__item--active')
+    })
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('slider__item--active')
-  })
-}
+slidesPlugin()
